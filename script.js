8908b962148fdb788d2b78984a30489ffe4ca859
Pablo Rocha Goncalves <pablorgoncalves@gmail.com>
2021-10-05 13:03:12 -0300

    Teste Git

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+    const body = document.getElementsByTagName('body')[0]
+    const section = document.createElement('section')
+    body.appendChild(section)
+    section.classList.add('sec')
+    section.setAttribute('id', 'sec')
+
+
+    
+
+
 const sec = document.getElementById("sec")
 
 const bola01 = document.createElement("div")
